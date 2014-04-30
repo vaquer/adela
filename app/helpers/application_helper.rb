44d4ef4711983d79210574622c33d88c0834680b
abigail sosa <abigail.sosa.h@gmail.com>
2014-04-30 15:05:08 -0500

    Actualizar versión de ruby a la 2.1.1 y versión de rails a la 4.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def bootstrap_flash_class(key)
     case key
-    when :notice then "success"
-    when :alert then "danger"
+    when "notice" then "success"
+    when "alert" then "danger"
     end
   end
 end

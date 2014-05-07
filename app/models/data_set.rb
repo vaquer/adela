b73a238bad1dead5fde811a184ccb56f29387da7
abigail sosa <abigail.sosa.h@gmail.com>
2014-05-07 13:08:09 -0500

    Cambia encoding a utf-8 en datasets

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -8,7 +8,9 @@ class DataSet
 
   def initialize(attributes = {})
     attributes.each do |name, value|
-      send("#{name}=", value)
+      if value.present?
+        send("#{name}=", value.force_encoding(Encoding::UTF_8))
+      end
     end
   end
 

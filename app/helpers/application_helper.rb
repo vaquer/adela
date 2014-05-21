cd042fd19ac33cea526e875a32cd3d9404cf13f7
Jorge Gajon <gajon@gajon.org>
2014-05-21 14:29:49 -0500

    [#71787968] CAMBIAR CONTRASEÑA: Como usuario puedo cambiar mi contraseña.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,23 @@ module ApplicationHelper
       "hidden"
     end
   end
+
+  def custom_devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    #sentence = I18n.t('errors.messages.not_saved',
+    #  count: resource.errors.count,
+    #  resource: resource.class.model_name.human.downcase)
+    sentence = "Corrige los siguientes errores:"
+
+    html = <<-HTML
+    <div class="alert alert-danger alert-block">
+      <h4>#{sentence}</h4>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Adela::Application.routes.draw do
   devise_for :users
 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    patch 'users/:id' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   get "/:slug/catalogo" => "organizations#catalog", :as => "organization_catalog"
   resources :users, only: :show
   root :to => "home#index"

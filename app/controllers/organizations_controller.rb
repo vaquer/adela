d8a12ea883abc37bc6f13ee1737e99c4af957451
abigail sosa <abigail.sosa.h@gmail.com>
2014-07-09 13:39:57 -0500

    Deleted authentication constraint on organizations#show

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,5 @@
 class OrganizationsController < ApplicationController
-  before_action :authenticate_user!, except: :catalog
+  before_action :authenticate_user!, except: [:catalog, :show]
 
   layout 'home'
 

f7a8fb6fa29d0532666be219d51b3a57c4996d1f
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-12-16 13:39:55 -0600

    Add back non-standard dcat:Distribution fields borrowed from dcat:Dataset

--- app/serializers/distribution_serializer.rb
+++ app/serializers/distribution_serializer.rb
@@ -1,5 +1,14 @@
 class DistributionSerializer < ActiveModel::Serializer
-  attributes :title, :description, :license, :downloadURL, :mediaType, :byteSize
+  attributes  :title,
+              :description,
+              :license,
+              :downloadURL,
+              :mediaType,
+              :byteSize,
+              # These are dcat:Dataset fields we're adding to dcat:Distribution
+              :temporal,
+              :spatial,
+              :accrualPeriodicity
 
   def license
     "http://datos.gob.mx/libreusomx/"

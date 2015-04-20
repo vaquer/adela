73ba4a1afdd5a9987d7347e4b10e47a2b838daea
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-04-20 17:36:37 -0500

    Testing error flow in InventoriesController

--- spec/controllers/inventories_controller_spec.rb
+++ spec/controllers/inventories_controller_spec.rb
@@ -27,36 +27,61 @@ describe InventoriesController do
         post :create, inventory: { csv_file: inventory_file }, :locale => "es"
         response.should render_template("new")
       end
+    end
 
+    context "ISO 8859-1 file" do
+      let(:inventory_file) { fixture_file_upload("files/inventory-latin-1.csv") }
+
+      it "creates a new inventory" do
+        post :create, inventory: { csv_file: inventory_file }, :locale => "es"
+        assigns(:inventory).should be_kind_of(Inventory)
+        assigns(:inventory).should be_persisted
+      end
+
+      it "assigns the inventory to the current organization" do
+        post :create, inventory: { csv_file: inventory_file }, :locale => "es"
+        assigns(:inventory).organization.should == user.organization
+      end
+
+      it "redirects to new inventory page" do
+        post :create, inventory: { csv_file: inventory_file }, :locale => "es"
+        response.should render_template("new")
+      end
+    end
+
+    context "file with invalid encoding" do
+      before do
+        def controller.create
+          raise Exceptions::UnknownEncodingError
+        end
+      end
+
+      it "redirects to inventories_path" do
+        post :create, inventory: { csv_file: "" }, :locale => "es"
+        expect(response).to redirect_to(inventories_path)
+      end
+
+      it "has 302 status" do
+        post :create, inventory: { csv_file: "" }, :locale => "es"
+        expect(response.status).to eq(302)
+      end
+
+      it "shows invalid encoding message" do
+        post :create, inventory: { csv_file: "" }, :locale => "es"
+        expect(flash[:alert]).to eq(I18n.t("activerecord.errors.models.inventory.attributes.csv_file.encoding"))
+      end
+
+      it "raises Exceptions::UnknownEncodingError" do
+        bypass_rescue
+        expect { post :create, inventory: { csv_file: "" }, :locale => "es" }.to raise_error(Exceptions::UnknownEncodingError)
+      end
+    end
+
+    context "empty inventory file" do
       it "renders the template again on error" do
         post :create, inventory: { csv_file: "" }, :locale => "es"
         response.should render_template("new")
       end
-    end
-  end
-
-  context "ISO 8859-1 file" do
-    let(:inventory_file) { fixture_file_upload("files/inventory-latin-1.csv") }
-
-    it "creates a new inventory" do
-      post :create, inventory: { csv_file: inventory_file }, :locale => "es"
-      assigns(:inventory).should be_kind_of(Inventory)
-      assigns(:inventory).should be_persisted
-    end
-
-    it "assigns the inventory to the current organization" do
-      post :create, inventory: { csv_file: inventory_file }, :locale => "es"
-      assigns(:inventory).organization.should == user.organization
-    end
-
-    it "redirects to new inventory page" do
-      post :create, inventory: { csv_file: inventory_file }, :locale => "es"
-      response.should render_template("new")
-    end
-
-    it "renders the template again on error" do
-      post :create, inventory: { csv_file: "" }, :locale => "es"
-      response.should render_template("new")
     end
   end
 end

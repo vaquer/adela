126f6318f50046d1708c8249ce10384a95b79428
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-04-29 23:43:04 -0500

    Adding csv validator based on csvlint

---
+++ app/validators/csv_file_validator.rb
@@ -0,0 +1,20 @@
+class CsvFileValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    validator = csv_validator(value)
+    unless validator.valid?
+      validator.errors.each do |error|
+        errors[:base] << message(error)
+      end
+    end
+  end
+
+  private
+  def csv_validator(csv)
+    csv_string = StringIO.new(csv.read.to_utf8)
+    Csvlint::Validator.new(csv_string) 
+  end
+
+  def message(error)
+    I18n.t("activerecord.errors.models.inventory.attributes.csv_file.#{error.type.to_s}", row: error.row)
+  end
+end

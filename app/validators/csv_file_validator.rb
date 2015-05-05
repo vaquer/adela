ea590ce955e025c1f83acb4b671f9ece301cc91b
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-05-05 00:05:28 -0500

    Adding empty file validation

--- app/validators/csv_file_validator.rb
+++ app/validators/csv_file_validator.rb
@@ -1,6 +1,15 @@
 class CsvFileValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    validator = csv_validator(value)
+    if value.blank?
+      record.errors[:base] << I18n.t("activerecord.errors.models.inventory.attributes.csv_file.blank_file")
+    else
+      csvlint_validations(record, value)
+    end
+  end
+
+  private
+  def csvlint_validations(record, value)
+    validator = csvlint_validator(value)
     unless validator.valid?
       validator.errors.each do |error|
         record.errors[:base] << message(error)
@@ -8,8 +17,7 @@ class CsvFileValidator < ActiveModel::EachValidator
     end
   end
 
-  private
-  def csv_validator(csv)
+  def csvlint_validator(csv)
     csv_string = StringIO.new(csv.read.to_utf8)
     Csvlint::Validator.new(csv_string) 
   end

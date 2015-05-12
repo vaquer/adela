ec8e768056b3871f00632c66360e7c4530171f13
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-05-12 11:29:02 -0500

    Removing csv processor generate method
    Removing dead code

--- spec/models/csv_processor_spec.rb
+++ spec/models/csv_processor_spec.rb
@@ -12,20 +12,4 @@ describe CsvProcessor do
       subject.process[1].distributions.size.should == 4
     end
   end
-
-  describe "#generate_csv" do
-    subject do
-      @temporary_path = "#{Rails.root}/tmp/inventory.csv"
-      @organization = FactoryGirl.create(:organization)
-      CsvProcessor.new(File.open("#{Rails.root}/spec/fixtures/files/partial_invalid_inventory.csv"), @organization)
-    end
-
-    it "creates new file only with valid datasets and distributions" do
-      subject.generate_csv(@temporary_path)
-      inventory = FactoryGirl.build(:inventory, :csv_file => File.open(@temporary_path), :organization => @organization)
-      inventory.should be_valid
-      inventory.datasets.size.should == 2
-      inventory.datasets[1].distributions.size.should == 0 
-    end
-  end
 end

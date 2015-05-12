0a0dc192385b37d2e55f6510939fb0556a060944
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-05-11 23:07:37 -0500

    Removing model tests that raised csv malformed exception

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 
 describe Inventory do
-  context "validations" do
+  context 'valid inventories' do
     before(:each) do
       @inventory = FactoryGirl.create(:inventory)
     end
 
-    shared_examples "an invalid inventory file" do
+    it 'should be valid with mandatory fields' do
+      @inventory.should be_valid
+    end
+
+    it 'should not be valid without an organization' do
+      @inventory.organization_id = nil
+      @inventory.should_not be_valid
+    end
+  end
+
+  context 'invalid inventories' do
+    shared_examples 'an invalid inventory file' do
       before(:each) do
-        @inventory.csv_file = File.new(csv_file)
+        file = File.new(csv_file)
+        @inventory = FactoryGirl.build(:inventory, csv_file: file)
       end
 
       it "should not be valid with an invalid file" do
@@ -38,27 +50,6 @@ describe Inventory do
       it_behaves_like "an invalid inventory file" do
         let(:csv_file) { "#{Rails.root}/spec/fixtures/files/blank_rows_inventory-2.csv" }
       end
-    end
-
-    context "file with stray quotes" do
-      it_behaves_like "an invalid inventory file" do
-        let(:csv_file) { "#{Rails.root}/spec/fixtures/files/stray_quote_inventory.csv" }
-      end
-    end
-
-    context "file with a quoted column has leading or trailing whitespace" do
-      it_behaves_like "an invalid inventory file" do
-        let(:csv_file) { "#{Rails.root}/spec/fixtures/files/whitespace_inventory.csv" }
-      end
-    end
-
-    it "should be valid with mandatory fields" do
-      @inventory.should be_valid
-    end
-
-    it "should not be valid without an organization" do
-      @inventory.organization_id = nil
-      @inventory.should_not be_valid
     end
   end
 end

17045b6fde5631736c2465e595350ffc4a4dd6c3
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-07-01 00:14:17 -0500

    Refactoring CsvProcessor
    Removes duplicated code

--- app/models/csv_processor.rb
+++ app/models/csv_processor.rb
@@ -18,11 +18,23 @@ class CsvProcessor < Struct.new(:csv_file, :organization)
   private
 
   def new_dataset(row)
-    version3?(row) ? new_dataset_v3(row) : new_dataset_v2(row)
+    dcat_v3?(row) ? build_dataset_v3(row) : build_dataset_v2(row)
   end
 
-  def new_dataset_v3(row)
-    DCAT::V3::DataSet.new({
+  def build_dataset_v3(row)
+    dataset_attributes = dataset_common_attributes(row)
+    dataset_attributes[:landingPage] = row["ds:landingPage"]
+    DCAT::V3::DataSet.new(dataset_attributes)
+  end
+
+  def build_dataset_v2(row)
+    dataset_attributes = dataset_common_attributes(row)
+    dataset_attributes[:dataDictionary] = row["ds:dataDictionary"]
+    DCAT::V2::DataSet.new(dataset_attributes)
+  end
+
+  def dataset_common_attributes(row)
+    {
       :identifier => I18n.transliterate((row["ds:identifier"] || row[0]).force_encoding('utf-8')),
       :title => row["ds:title"],
       :description => row["ds:description"],
@@ -34,29 +46,9 @@ class CsvProcessor < Struct.new(:csv_file, :organization)
       :accessLevelComment => row["ds:accessLevelComment"],
       :temporal => row["ds:temporal"],
       :spatial => row["ds:spatial"],
-      :landingPage => row["ds:landingPage"],
       :accrualPeriodicity => row["accrualPeriodicity"],
       :publisher => organization.title,
-    })
-  end
-
-  def new_dataset_v2(row)
-    DCAT::V2::DataSet.new({
-      :identifier => I18n.transliterate((row["ds:identifier"] || row[0]).force_encoding('utf-8')),
-      :title => row["ds:title"],
-      :description => row["ds:description"],
-      :keyword => row["ds:keyword"],
-      :modified => row["ds:modified"],
-      :contactPoint => row["ds:contactPoint"],
-      :mbox => row["ds:mbox"],
-      :accessLevel => row["ds:accessLevel"],
-      :accessLevelComment => row["ds:accessLevelComment"],
-      :temporal => row["ds:temporal"],
-      :spatial => row["ds:spatial"],
-      :dataDictionary => row["ds:dataDictionary"],
-      :accrualPeriodicity => row["accrualPeriodicity"],
-      :publisher => organization.title,
-    })
+    }
   end
 
   def new_distribution(row)
@@ -82,7 +74,7 @@ class CsvProcessor < Struct.new(:csv_file, :organization)
 
   private
 
-  def version3?(row)
+  def dcat_v3?(row)
     row.header?("ds:landingPage")
   end
 end

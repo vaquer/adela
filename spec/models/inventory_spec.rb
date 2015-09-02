288f19b0ae5907b0c669c100f801b63141a95b7f
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-02 15:15:48 -0500

    Refactoring over inventory spec
    Keeps specs DRY with shared examples.

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,28 +1,31 @@
 require 'spec_helper'
 
 describe Inventory do
-  context 'with all mandatory fields' do
-    let(:inventory) { build_stubbed(:inventory) }
-
+  shared_examples 'a valid inventory' do
     it 'should be valid ' do
       inventory.should be_valid
     end
   end
 
-  context 'without an spreadsheet file' do
-    let(:inventory) { build_stubbed(:inventory, spreadsheet_file: nil) }
-
+  shared_examples 'an invalid inventory' do
     it 'should not be valid ' do
       inventory.should_not be_valid
     end
   end
 
+  context 'with all mandatory fields' do
+    let(:inventory) { build_stubbed(:inventory) }
+    it_behaves_like 'a valid inventory'
+  end
+
+  context 'without an spreadsheet file' do
+    let(:inventory) { build_stubbed(:inventory, spreadsheet_file: nil) }
+    it_behaves_like 'an invalid inventory'
+  end
+
   context 'without an spreadsheet file' do
     let(:inventory) { build_stubbed(:inventory, organization: nil) }
-
-    it 'should not be valid ' do
-      inventory.should_not be_valid
-    end
+    it_behaves_like 'an invalid inventory'
   end
 
   context 'after saving an inventory' do

67da6b1ccb27d7358ccfc55d31e7fbcd46e6aaed
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-02 05:42:02 -0500

    Updates InventoryXLSXParser
    InventoryXLSXParser class now parses the xlsx file to instances of InventoryElement (the ActiveRecord version of InventoryRow). Also adds compliant validations at spec.

--- app/models/inventory_xlsx_parser.rb
+++ app/models/inventory_xlsx_parser.rb
@@ -10,7 +10,7 @@ class InventoryXLSXParser
     xlsx = Roo::Spreadsheet.open(inventory_file_path)
     (xlsx.first_row..xlsx.last_row).drop(1).map do |i|
       xlsx_row = xlsx.row(i)
-      build_inventory_row(xlsx_row, i)
+      create_inventory_element(xlsx_row, i)
     end
   end
 
@@ -20,21 +20,30 @@ class InventoryXLSXParser
     Rails.env.production? ? @inventory.spreadsheet_file.url : @inventory.spreadsheet_file
   end
 
-  def build_inventory_row(xlsx_row, row_number)
-    InventoryRow.new(
-      number: row_number,
-      responsible: xlsx_row[0],
-      dataset_title: xlsx_row[1],
-      resource_title: xlsx_row[2],
-      dataset_description: xlsx_row[3],
-      private_data: parse_private_data(xlsx_row),
-      data_access_comment: xlsx_row[5],
-      media_type: xlsx_row[6],
-      publish_date: xlsx_row[7].to_s
-    )
+  def create_inventory_element(xlsx_row, row)
+    @inventory.inventory_elements.create do |element|
+      element.row = row
+      element.responsible = xlsx_row[0]
+      element.dataset_title = xlsx_row[1]
+      element.resource_title = xlsx_row[2]
+      element.description = xlsx_row[3]
+      element.private = parse_private_data(xlsx_row)
+      element.access_comment = xlsx_row[5]
+      element.media_type = xlsx_row[6]
+      element.publish_date = parse_publish_date(xlsx_row)
+    end
   end
 
   def parse_private_data(xlsx_row)
     xlsx_row[4] =~ /P[úu]blico/i ? false : true
+  end
+
+  def parse_publish_date(xlsx_row)
+    publish_date = xlsx_row[7].to_s
+    publish_date if matches_iso8601?(publish_date)
+  end
+
+  def matches_iso8601?(publish_date)
+    publish_date =~ /^(\d{4})-(\d{2})/
   end
 end

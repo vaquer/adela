a4325cab7f66337498dfcfb1b1e92f9f3111bc53
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-04 00:46:24 -0500

    Fixes broken tests
    Test used old opening plan flow.

--- spec/requests/opening_plan_management_spec.rb
+++ spec/requests/opening_plan_management_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 feature "opening plan management" do
   background do
-    file = File.new("#{Rails.root}/spec/fixtures/files/catalog-with-opening-plan.csv")
-    @catalog = FactoryGirl.create(:published_catalog, csv_file: file)
+    @opening_plan = create(:opening_plan_with_officials)
   end
 
   it "returns the organization opening plan" do
-    get "/#{@catalog.organization.slug}/plan.json"
+    get "/#{@opening_plan.organization.slug}/plan.json"
     expect(response).to match_response_schema("opening_plan")
   end
 end

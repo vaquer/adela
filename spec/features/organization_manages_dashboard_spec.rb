e4ed52d345fa38602bae615dd3d2c5d91c2dd277
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-05 11:46:37 -0500

    Removes unused test
    Users cant publish anymore partial catalogs.

--- spec/features/organization_manages_dashboard_spec.rb
+++ spec/features/organization_manages_dashboard_spec.rb
@@ -24,19 +24,6 @@ feature Organization, 'manages dashboard:' do
     expect(page).to have_text "#{I18n.l(10.days.ago, :format => :short)}"
   end
 
-  scenario "can publish last catalog version", :js => true do
-    given_has_uploaded_an_catalog 5.days.ago
-    given_has_published_an_catalog 10.days.ago
-    visit organization_path(@organization)
-    click_on "Actualizar catálogo"
-    expect(page).to have_text "La versión publicada no es la última versión."
-    click_on "Publicar última versión"
-    expect(page).to have_text "Paso 5"
-    check_publication_requirements
-    click_on "Publicar"
-    page.should_not have_content("Publicar última versión")
-  end
-
   def given_has_uploaded_an_catalog(days_ago)
     @catalog = FactoryGirl.create(:catalog)
     @catalog.update_attributes(:author => @user.name, :organization_id => @organization.id, :created_at => days_ago)

a92793e25a220610d37aee2d45636940546e44ed
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-09 17:56:19 -0500

    Creates Opening Plan Helpers for accrual periodicity
    Default values will be stored at config/iso8601.yml and available in ISO8601 constant.

---
+++ app/helpers/opening_plans_helper.rb
@@ -0,0 +1,15 @@
+module OpeningPlansHelper
+  def iso8601_repeating_interval_options_for_select(default)
+    options_for_select(iso8601_options, default)
+  end
+
+  def parse_iso8601(iso8601)
+    ISO8601['accrual_periodicity'].key(iso8601)
+  end
+
+  private
+
+  def iso8601_options
+    ISO8601['accrual_periodicity'].to_a
+  end
+end

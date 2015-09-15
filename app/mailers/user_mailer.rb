f4b39cf9d56db7b89fee92e4675ada8dcff4b2d1
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-15 18:38:12 -0500

    Uses MAILER_FROM env variable
    Removes hardcoded value.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
-#encoding: utf-8;
+# encoding: utf-8
 class UserMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
 
   def notificate_user_account(user, password)
     @user = user
     @password = password
-    to = @user.email
-    mail(:to => to, :from => "no-reply@adela.com", :subject => I18n.t('mailers.user.notificate_user_account_subject')).deliver
+    mail(to: @user.email, from: MAILER_FROM, subject: I18n.t('mailers.user.notificate_user_account_subject')).deliver
   end
 end

7a256a224de7f83133af5e595ec438b0989050ef
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-22 13:57:49 -0500

    Updates sector model
    If a sector is destroyed, it deletes the dependent organization_sectors.

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,5 +1,5 @@
 class Sector < ActiveRecord::Base
-  has_many :organization_sectors
+  has_many :organization_sectors, dependent: :destroy
   has_many :organizations, through: :organization_sectors
   validates :title, presence: true
 end

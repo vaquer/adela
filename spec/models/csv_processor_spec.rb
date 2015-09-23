bf5df725dbc38346f3600c4a8824374846e39a71
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-23 16:12:27 -0500

    Updates csv processor specs
    Adds test case for the organization sector in dataset keywords.

--- spec/models/csv_processor_spec.rb
+++ spec/models/csv_processor_spec.rb
@@ -2,14 +2,30 @@ require 'spec_helper'
 
 describe CsvProcessor do
   describe "#process" do
-    subject do
-      CsvProcessor.new(FactoryGirl.create(:catalog).csv_file, FactoryGirl.create(:organization))
+    context "an organization catalog" do
+      subject do
+        CsvProcessor.new(FactoryGirl.create(:catalog).csv_file, FactoryGirl.create(:organization))
+      end
+
+      it "gets the datasets from the file" do
+        subject.process.size.should == 2
+        subject.process[0].distributions.size.should == 3
+        subject.process[1].distributions.size.should == 4
+      end
     end
 
-    it "gets the datasets from the file" do
-      subject.process.size.should == 2
-      subject.process[0].distributions.size.should == 3
-      subject.process[1].distributions.size.should == 4
+    context "an organization with sectors catalog" do
+      subject do
+        organization = create(:organization, :sector)
+        csv_file = create(:catalog).csv_file
+        CsvProcessor.new(csv_file, organization)
+      end
+
+      it "contains the organization sectors in the keywords" do
+        sector_keyword = subject.organization.sectors.first.title
+        includes_sector = subject.process[0].keywords.include?(sector_keyword)
+        expect(includes_sector).to be true
+      end
     end
   end
 end

64720ea30dcb07c67d786c6fb1c1e24292997e67
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-23 12:56:17 -0500

    Improves style
    - Uses Ruby 1.9 hash syntax.
    - Replaces double quotes with single quotes when no string interpolation is needed.
    - Removes redundant curly braces
    - Removes commas from last hash parameter

--- app/models/csv_processor.rb
+++ app/models/csv_processor.rb
@@ -4,7 +4,7 @@ class CsvProcessor < Struct.new(:csv_file, :organization)
   def process
     datasets = []
     if csv_file.url.present?
-      CSV.new(csv_file.read.to_utf8, :headers => :first_row).each do |row|
+      CSV.new(csv_file.read.to_utf8, headers: :first_row).each do |row|
         if dataset?(row)
           datasets << new_dataset(row)
         elsif distribution?(row)
@@ -23,57 +23,55 @@ class CsvProcessor < Struct.new(:csv_file, :organization)
 
   def build_dataset_v3(row)
     dataset_attributes = dataset_common_attributes(row)
-    dataset_attributes[:landingPage] = row["ds:landingPage"]
+    dataset_attributes[:landingPage] = row['ds:landingPage']
     DCAT::V3::DataSet.new(dataset_attributes)
   end
 
   def build_dataset_v2(row)
     dataset_attributes = dataset_common_attributes(row)
-    dataset_attributes[:dataDictionary] = row["ds:dataDictionary"]
+    dataset_attributes[:dataDictionary] = row['ds:dataDictionary']
     DCAT::V2::DataSet.new(dataset_attributes)
   end
 
   def dataset_common_attributes(row)
     {
-      :identifier => I18n.transliterate((row["ds:identifier"] || row[0]).force_encoding('utf-8')),
-      :title => row["ds:title"],
-      :description => row["ds:description"],
-      :keyword => row["ds:keyword"],
-      :modified => row["ds:modified"],
-      :contactPoint => row["ds:contactPoint"],
-      :mbox => row["ds:mbox"],
-      :accessLevel => row["ds:accessLevel"],
-      :accessLevelComment => row["ds:accessLevelComment"],
-      :temporal => row["ds:temporal"],
-      :spatial => row["ds:spatial"],
-      :accrualPeriodicity => row["ds:accrualPeriodicity"],
-      :publisher => organization.title,
+      identifier: I18n.transliterate((row['ds:identifier'] || row[0]).force_encoding('utf-8')),
+      title: row['ds:title'],
+      description: row['ds:description'],
+      keyword: row['ds:keyword'],
+      modified: row['ds:modified'],
+      contactPoint: row['ds:contactPoint'],
+      mbox: row['ds:mbox'],
+      accessLevel: row['ds:accessLevel'],
+      accessLevelComment: row['ds:accessLevelComment'],
+      temporal: row['ds:temporal'],
+      spatial: row['ds:spatial'],
+      accrualPeriodicity: row['ds:accrualPeriodicity'],
+      publisher: organization.title
     }
   end
 
   def new_distribution(row)
-    Distribution.new({
-      :title => row["rs:title"],
-      :description => row["rs:description"],
-      :downloadURL => row["rs:downloadURL"],
-      :mediaType => row["rs:mediaType"],
-      :byteSize => row["rs:byteSize"],
-      :temporal => row["rs:temporal"],
-      :spatial => row["rs:spatial"],
-    })
+    Distribution.new(
+      title: row['rs:title'],
+      description: row['rs:description'],
+      downloadURL: row['rs:downloadURL'],
+      mediaType: row['rs:mediaType'],
+      byteSize: row['rs:byteSize'],
+      temporal: row['rs:temporal'],
+      spatial: row['rs:spatial']
+    )
   end
 
   def dataset?(row)
-    (row["ds:identifier"].present? || row[0].present?) && row["ds:title"].present?
+    (row['ds:identifier'].present? || row[0].present?) && row['ds:title'].present?
   end
 
   def distribution?(row)
-    (row["ds:identifier"].present? || row[0].present?) && row["rs:title"].present?
+    (row['ds:identifier'].present? || row[0].present?) && row['rs:title'].present?
   end
 
-  private
-
   def dcat_v3?(row)
-    row.header?("ds:landingPage")
+    row.header?('ds:landingPage')
   end
 end

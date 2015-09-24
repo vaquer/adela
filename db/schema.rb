b1574fb5a8dfc29656722db4bd2f469d4f9b8c50
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-09-23 23:34:45 -0500

    Updates inventory schema
    Adds error_message column to inventory.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924023744) do
+ActiveRecord::Schema.define(version: 20150924031729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150924023744) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authorization_file"
+    t.string   "error_message"
   end
 
   add_index "inventories", ["organization_id"], name: "index_inventories_on_organization_id", using: :btree

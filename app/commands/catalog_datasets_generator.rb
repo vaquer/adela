cec89018c3feac1aaa1745286cbdbf5f49d7e6f4
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-12 13:20:26 -0500

    Creates the Catalog Datasets Generator Command
    Generates the catalog datasets.

---
+++ app/commands/catalog_datasets_generator.rb
@@ -0,0 +1,52 @@
+class CatalogDatasetsGenerator
+  attr_reader :organization, :inventory
+
+  def initialize(organization)
+    @organization = organization
+    @inventory = organization.inventories.order(created_at: :desc).find(&:compliant?)
+  end
+
+  def execute
+    build_datasets.each { |dataset| build_distributions(dataset) }
+    @organization.save
+  end
+
+  private
+
+  def build_datasets
+    @organization.opening_plans.map do |opening_plan|
+      opening_plan_to_dataset(opening_plan)
+    end
+  end
+
+  def opening_plan_to_dataset(opening_plan)
+    @organization.catalog.datasets.create(
+      identifier: opening_plan.name.downcase.split.join('-'),
+      title: opening_plan.name,
+      description: opening_plan.description,
+      contact_point: organization_administrator.try(:name),
+      mbox: organization_administrator.try(:email),
+      # TODO: add dct:landing_page field
+      # landing_page: Faker::Internet.url,
+      accrual_periodicity: opening_plan.accrual_periodicity
+    )
+  end
+
+  def build_distributions(dataset)
+    select_distributions(dataset).map do |distribution|
+      dataset.distributions.create(
+        title: distribution.resource_title,
+        description: distribution.description,
+        media_type: distribution.media_type
+      )
+    end
+  end
+
+  def select_distributions(dataset)
+    @inventory.inventory_elements.select { |element| element.dataset_title == dataset.title }
+  end
+
+  def organization_administrator
+    @organization.administrator.try(:user)
+  end
+end

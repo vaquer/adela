c04bb6c6edced56f0dd260a3d602467005c5773c
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-13 10:56:40 -0500

    Updates dataset model
    Adds publish date field.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013035926) do
+ActiveRecord::Schema.define(version: 20151013144129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20151013035926) do
     t.integer  "catalog_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "publish_date"
   end
 
   add_index "datasets", ["catalog_id"], name: "index_datasets_on_catalog_id", using: :btree

a391866a4267abf1486063dfe12b1614d5f83c6a
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-14 21:57:45 -0500

    Updates datsets model
    Adds contact_position field to dataset.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013154250) do
+ActiveRecord::Schema.define(version: 20151015025503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20151013154250) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "publish_date"
+    t.string   "contact_position"
   end
 
   add_index "datasets", ["catalog_id"], name: "index_datasets_on_catalog_id", using: :btree

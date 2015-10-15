ccb67d82f8c1cf0c2492a6f7c1f243293ab9274e
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-15 16:55:39 -0500

    Updates datasets controller
    Adds the datasets#edit action.

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -4,4 +4,28 @@ class DatasetsController < ApplicationController
   def index
     @catalog = current_organization.catalog
   end
+
+  def edit
+    @dataset = Dataset.find(params['id'])
+    @dataset.dataset_sector || @dataset.build_dataset_sector
+  end
+
+  def update
+    @dataset = Dataset.find(params['id'])
+    @dataset.update(dataset_params)
+    render nothing: true
+    return
+  end
+
+  private
+
+  def dataset_params
+    params.require(:dataset).permit(
+      :contact_position,
+      :mbox,
+      :landing_page,
+      :keyword,
+      dataset_sector_attributes: [:sector_id]
+    )
+  end
 end

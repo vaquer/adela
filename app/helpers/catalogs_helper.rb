a1d23c76f2df280b7a3b8a7a267c30868a70cfb1
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-16 13:54:09 -0500

    Adds the published_catalog_id helper method.
    It returns the folio for a published catalog.

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -2,4 +2,10 @@ module CatalogsHelper
   def can_publish?(catalog)
     catalog.datasets.map(&:distributions).flatten.map(&:state).grep('validated').present?
   end
+
+  def published_catalog_id(catalog)
+    upcase_slug = @catalog.organization.slug.upcase
+    epoch_time = @catalog.publish_date.to_i
+    "#{upcase_slug}-#{epoch_time}"
+  end
 end

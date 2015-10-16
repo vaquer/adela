4f5d251ad5208c26b7ec94aacedf7c91736a8832
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-16 02:49:06 -0500

    Updates the Distribution Model validations
    Adds compliant validations to Distribution Model

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,3 +1,11 @@
 class Distribution < ActiveRecord::Base
   belongs_to :dataset
+  validate :mandatory_fields
+
+  def mandatory_fields
+    fields = %i(download_url temporal modified)
+    fields.each do |field|
+      warnings.add(field) if send(field).blank?
+    end
+  end
 end
--- spec/models/distribution_spec.rb
+++ spec/models/distribution_spec.rb
@@ -2,13 +2,45 @@ require 'spec_helper'
 
 describe Distribution do
   shared_examples 'a valid distribution' do
-    it 'should be valid ' do
-      distribution.should be_valid
+    it 'should be valid' do
+      expect(distribution).to be_valid
+    end
+  end
+
+  shared_examples 'a compliant distribution' do
+    it 'should be compliant' do
+      expect(distribution.compliant?).to be_true
+    end
+  end
+
+  shared_examples 'a non compliant distribution' do
+    it 'should be non compliant' do
+      expect(distribution.compliant?).to be_false
     end
   end
 
   context 'with all mandatory fields' do
     let(:distribution) { create(:distribution) }
     it_behaves_like 'a valid distribution'
+  end
+
+  context 'with all fields' do
+    let(:distribution) { create(:distribution) }
+    it_behaves_like 'a compliant distribution'
+  end
+
+  context 'with a nil download_url' do
+    let(:distribution) { create(:distribution, download_url: nil) }
+    it_behaves_like 'a non compliant distribution'
+  end
+
+  context 'with a nil temporal' do
+    let(:distribution) { create(:distribution, temporal: nil) }
+    it_behaves_like 'a non compliant distribution'
+  end
+
+  context 'with a nil modified' do
+    let(:distribution) { create(:distribution, modified: nil) }
+    it_behaves_like 'a non compliant distribution'
   end
 end

341fe69ee67d88c93dce98526e0b3e22a2da316b
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-18 14:29:18 -0500

    Removes helper from datasets helper
    Removes published_distributions_percentage helper.

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -1,12 +1,4 @@
 module DatasetsHelper
-  def published_distributions_percentage(dataset)
-    total = dataset.distributions.count
-    published = dataset.distributions.where(state: 'published').count
-    (published.to_f / total * 100).to_i
-  rescue
-    0
-  end
-
   def accrual_periodicity_translate(accrual_periodicity)
     ISO8601_DEFAULTS['accrual_periodicity'].invert[accrual_periodicity]
   end
--- spec/helpers/datasets_helper_spec.rb
+++ spec/helpers/datasets_helper_spec.rb
@@ -1,26 +1,6 @@
 require 'spec_helper'
 
 describe DatasetsHelper do
-  describe '#published_distributions_percentage' do
-
-    before(:each) do
-      @dataset = FactoryGirl.create(:dataset)
-    end
-
-    it 'returns percentage of published distributions' do
-      FactoryGirl.create(:distribution, dataset: @dataset)
-      distribution = FactoryGirl.create(:distribution, dataset: @dataset)
-      distribution.update_column(:state, 'published')
-      percentage = helper.published_distributions_percentage(@dataset)
-      expect(percentage).to eq(50)
-    end
-
-    it 'returns 0 when dataset has no distributions' do
-      percentage = helper.published_distributions_percentage(@dataset)
-      expect(percentage).to eq(0)
-    end
-  end
-
   describe '#accrual_periodicity_translate' do
     it 'translates the accrual periodicity values' do
       ISO8601_DEFAULTS['accrual_periodicity'].each do |key, value|

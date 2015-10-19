fcad433ddcbed33c63ca14c6ffcfa0425948d93a
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-19 15:20:17 -0500

    Updates users factory
    Adds the administrator trait.

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,6 +10,12 @@ FactoryGirl.define do
     factory :admin do
       after(:create) { |user| user.add_role(:admin) }
     end
+
+    trait :administrator do
+      after(:create) do |user|
+        create(:administrator, user: user, organization: user.organization)
+      end
+    end
   end
 
   factory :published_catalog, :parent => :catalog do |f|

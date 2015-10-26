86d4231b2b9d08124a936e80854d38556785b226
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-10-26 11:09:47 -0700

    Fix failing test for inventory elements on catalog

--- spec/features/user_sees_inventory_elements_spec.rb
+++ spec/features/user_sees_inventory_elements_spec.rb
@@ -7,12 +7,13 @@ feature User, 'sees inventory elements:' do
   end
 
   scenario 'when inventory file has been uploaded' do
-    given_organization_with_catalog
+    create :sector, title: "Custom sector"
+    given_organization_with_ready_catalog
     click_link 'Catálogo de Datos'
 
     within set_row do
       expect(page).to have_content "Inventario Institucional de Datos"
-      expect(page).to have_content "28-Ago-2015"
+      expect(page).to have_content "28-Ago-15"
       expect(page).to have_content "1 de 1"
       expect(page).to have_link "Editar"
     end
@@ -42,11 +43,41 @@ feature User, 'sees inventory elements:' do
     InventoryXLSXParserWorker.new.perform(@inventory.id)
   end
 
-  def given_organization_with_catalog
-    upload_inventory_with_file("inventario_general_de_datos.xlsx")
+  def generate_new_opening_plan
     visit new_opening_plan_path
     fill_in 'organization_opening_plans_attributes_0_description', with: 'osom dataset'
     select('anual', from: 'organization[opening_plans_attributes][0][accrual_periodicity]')
     click_on('Generar Plan de Apertura')
+  end
+
+  def edit_catalog_first_set
+    click_link "Catálogo de Datos"
+    within set_row do
+      click_link "Editar"
+    end
+
+    fill_in "Correo del responsable", with: @user.email
+    select "Custom sector", from: "dataset_dataset_sector_attributes_sector_id"
+    click_link "Volver al catálogo"
+  end
+
+  def complete_first_resource_set
+    click_link "Catálogo de Datos"
+    within resource_row do
+      click_link "Completar"
+    end
+    fill_in "URL para descargar", with: "http://www.fakeurl.com"
+    fill_in "init-date", with: "2015"
+    fill_in "term-date", with: "2015-12-20"
+    fill_in "distribution_modified", with: "2015-08-28"
+    click_button "Guardar avance"
+    click_link "Volver al catálogo"
+  end
+
+  def given_organization_with_ready_catalog
+    upload_inventory_with_file("inventario_general_de_datos.xlsx")
+    generate_new_opening_plan
+    edit_catalog_first_set
+    complete_first_resource_set
   end
 end

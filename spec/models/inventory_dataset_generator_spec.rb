a722d44619c7c89ca1ac792d47ea452635e3ba51
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-10-26 02:53:41 -0600

    Updates the inventory dataset when the inventory is updated
    Updates the existing inventory dataset, instead of creating a new one.

--- spec/models/inventory_dataset_generator_spec.rb
+++ spec/models/inventory_dataset_generator_spec.rb
@@ -1,38 +1,77 @@
 require 'spec_helper'
 
 describe InventoryDatasetGenerator do
-  before(:each) do
-    @inventory = create(:inventory, :elements)
-  end
-
   describe '#generate' do
-    before(:each) do
-      InventoryDatasetGenerator.new(@inventory).generate
+    context 'a new inventory' do
+      let(:organization) { create(:organization) }
+      let(:inventory) { create(:inventory, :elements, organization: organization) }
+
+      before(:each) do
+        InventoryDatasetGenerator.new(inventory).generate
+      end
+
+      it 'should create an organization catalog' do
+        expect(organization.catalog).not_to be_nil
+      end
+
+      it 'should create a dataset' do
+        expect(organization.catalog.datasets.count).to eql(1)
+      end
+
+      it 'should create a dataset with one distribution' do
+        dataset = organization.catalog.datasets.last
+        expect(dataset.distributions.count).to eql(1)
+      end
+
+      it 'should contain an identifier with the organization slug' do
+        organization_slug = organization.slug
+        dataset_identifier = organization.catalog.datasets.last.identifier
+        expected_identifier = "#{organization_slug}-inventario-institucional-de-datos"
+
+        expect(dataset_identifier).to eql(expected_identifier)
+      end
     end
 
-    it 'should generate an organization catalog' do
-      organization = @inventory.organization
-      expect(organization.catalog).not_to be_nil
-    end
+    context 'an existing inventory' do
+      before(:each) do
+        @organization = create(:organization)
+        old_inventory = create(:inventory, organization: @organization)
 
-    it 'should generate a dataset' do
-      catalog = @inventory.organization.catalog
-      expect(catalog.datasets.count).to eql(1)
-    end
+        InventoryXLSXParserWorker.new.perform(old_inventory.id)
+        @old_dataset = @organization.catalog.datasets.first.deep_clone(include: [:distributions])
 
-    it 'should generate a dataset with one distribution' do
-      catalog = @inventory.organization.catalog
-      dataset = catalog.datasets.last
-      expect(dataset.distributions.count).to eql(1)
-    end
+        Timecop.travel(Faker::Date.forward)
 
-    it 'should contain an identifier with the organization slug' do
-      catalog = @inventory.organization.catalog
-      organization_slug = catalog.organization.slug
-      dataset_identifier = catalog.datasets.last.identifier
-      expected_identifier = "#{organization_slug}-inventario-institucional-de-datos"
+        new_inventory = create(:inventory, organization: @organization)
+        InventoryDatasetGenerator.new(new_inventory).generate
+        @new_dataset = @organization.catalog.datasets.first.deep_clone(include: [:distributions])
+      end
 
-      expect(dataset_identifier).to eql(expected_identifier)
+      after(:each) do
+        Timecop.return
+      end
+
+      it 'should not create a new dataset' do
+        expect(@organization.catalog.datasets.count).to eq(1)
+      end
+
+      it 'should update the modified field from the existing dataset' do
+        old_dataset_modified = @old_dataset.modified
+        new_dataset_modified = @new_dataset.modified
+        expect(new_dataset_modified).to be > old_dataset_modified
+      end
+
+      it 'should update the modified field from the existing distribution' do
+        old_distribution_modified = @old_dataset.distributions.first.modified
+        new_distribution_modified = @new_dataset.distributions.first.modified
+        expect(new_distribution_modified).not_to eql(old_distribution_modified)
+      end
+
+      it 'should update the download_url field from the existing distribution' do
+        old_distribution_download_url = @old_dataset.distributions.first.download_url
+        new_distribution_download_url = @new_dataset.distributions.first.download_url
+        expect(new_distribution_download_url).not_to eql(old_distribution_download_url)
+      end
     end
   end
 end

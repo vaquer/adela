dfadee9bba596f902b0d80843976b3672bc16044
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-10-26 17:27:34 -0700

    Removed redundant spec steps for update inventory spec

--- spec/features/user_updates_inventory_spec.rb
+++ spec/features/user_updates_inventory_spec.rb
@@ -52,31 +52,50 @@ feature User, 'updates inventory:' do
     expect(first_set).to include "Tipos de vegetación"
   end
 
-  scenario 'and can not see removed resources from inventory' do
-    upload_inventory_with_file("inventario_general_de_datos_update.xlsx")
+  scenario 'and dataset metadata changes' do
+    upload_inventory_with_file("inventory-issue-398.xlsx")
     generate_new_opening_plan
+    inventory_dataset = @user.organization.catalog.distributions.sort.first
 
-    click_link "Catálogo de Datos"
-    expect(page).to have_content "Tipos de vegetación"
-    expect(page).to have_content "bosque de encinos, bosquie de coniferas, selva prenifolia, etc"
-    expect(page).to have_content "Desierto"
+    visit edit_distribution_path(inventory_dataset)
+    expect(download_url).to include "inventory-issue-398.xlsx"
+    expect(modified_date).to be_blank
+
+    visit edit_dataset_path(inventory_dataset.dataset)
+    expect(page).to have_content "irregular"
 
     visit new_inventory_path
 
     attach_file('inventory_spreadsheet_file', "#{Rails.root}/spec/fixtures/files/inventario_general_de_datos.xlsx")
     click_button "Subir inventario"
     InventoryXLSXParserWorker.new.perform(Inventory.last.id)
-    @user.organization.reload
-    generate_new_opening_plan
 
-    click_link "Catálogo de Datos"
-    expect(page).to have_content "Tipos de vegetación"
-    expect(page).to have_content "bosque de encinos, bosquie de coniferas, selva prenifolia, etc"
-    expect(page).not_to have_content "Desierto"
+    visit edit_distribution_path(inventory_dataset)
+    expect(download_url).to include "inventario_general_de_datos.xlsx"
+    expect(download_url).not_to include "inventory-issue-398.xlsx"
+    expect(modified_date).not_to be_blank
+  end
+
+  def download_url
+    find("#distribution_download_url").value
+  end
+
+  def modified_date
+    find("#distribution_modified").value
   end
 
   def first_set
     find_field("organization_opening_plans_attributes_0_name").value
+  end
+
+  def last_resource
+    all("tr.distribution").last
+  end
+
+  def click_last_dataset_edit
+    within last_resource do
+      click_link "Editar"
+    end
   end
 
   def upload_inventory_with_file(file_name)

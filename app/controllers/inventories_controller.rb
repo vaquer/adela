7bc7fa0ee4e21ed66cf032d804b4a50f7b055645
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-13 09:06:41 -0600

    Removes unused methods from inventories controller

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,24 +1,9 @@
 class InventoriesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    redirect_to new_inventory_path unless current_inventory
-    @inventory = current_inventory
-    @new_inventory = Inventory.new
-  end
-
-  def new
-    @inventory = Inventory.new
-  end
-
   def create
-    @inventory = current_organization.inventories.build(inventory_params)
-    @inventory.save
-    redirect_to inventory_path(@inventory)
-  end
-
-  def show
-    @inventory = Inventory.find(params[:id])
+    current_organization.inventories.create(inventory_params)
+    redirect_to inventories_path
   end
 
   private

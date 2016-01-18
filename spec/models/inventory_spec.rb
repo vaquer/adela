a572c5c2c78abb1d27b11118f0d1b4b5f058577d
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-18 10:46:14 -0600

    Fixes the inventory model spec

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Inventory do
   shared_examples 'a valid inventory' do
-    it 'should be valid ' do
+    it 'should be valid' do
       expect(inventory).to be_valid
     end
   end
@@ -23,7 +23,7 @@ describe Inventory do
     it_behaves_like 'an invalid inventory'
   end
 
-  context 'without an spreadsheet file' do
+  context 'without an organization' do
     let(:inventory) { build_stubbed(:inventory, organization: nil) }
     it_behaves_like 'an invalid inventory'
   end
@@ -31,13 +31,8 @@ describe Inventory do
   context 'after creating an inventory' do
     let(:inventory) { create(:inventory) }
 
-    it 'should be an InventoryXLSXParserWorker enqueued job' do
-      expect { inventory.run_callbacks(:commit) }.to change { InventoryXLSXParserWorker.jobs.count }.by(1)
-    end
-
-    it 'should be an InventorySpreadsheetFileWorker enqueued job' do
-      expect { inventory.run_callbacks(:commit) }.to change { InventorySpreadsheetFileWorker
-        .jobs.count }.by(1)
+    it 'should be an InventoryDatasetsWorker enqueued job' do
+      expect { inventory.run_callbacks(:commit) }.to change { InventoryDatasetsWorker.jobs.count }.by(1)
     end
   end
 end

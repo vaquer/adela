834144a7074f77d5550b2f719592514a0cd5c57b
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-17 22:49:16 -0600

    Adds the Inventories::Distribution edit and update actions

--- app/controllers/concerns/distribution_actions.rb
+++ app/controllers/concerns/distribution_actions.rb
@@ -9,4 +9,14 @@ module DistributionActions
     @distribution = @dataset.distributions.create(distribution_params)
     create_customization if create_customization.present?
   end
+
+  def edit
+    @distribution = Distribution.find(params['id'])
+  end
+
+  def update
+    @distribution = Distribution.find(params['id'])
+    @distribution.update(distribution_params)
+    update_customization if update_customization.present?
+  end
 end

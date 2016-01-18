8e20a43c54f9e6c1dd31f36ce8dbbc8551b25d1f
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-18 08:53:48 -0600

    Removes the old opening plan implementation

--- app/controllers/opening_plan_controller.rb
+++ app/controllers/opening_plan_controller.rb
@@ -12,20 +12,6 @@ class OpeningPlanController < ApplicationController
     @organization = current_organization
   end
 
-  def create
-    @organization = current_organization
-    @organization.opening_plans = []
-    @organization.update(organization_params)
-    create_opening_plan_officials
-    generate_catalog_datasets
-    generate_opening_plan_dataset
-    redirect_to opening_plan_index_path
-  end
-
-  def organization
-    @organization = Organization.find(params[:id])
-  end
-
   def export
     organization = Organization.find(params[:id])
     exporter = OpeningPlanExporter.new(organization)
@@ -36,52 +22,7 @@ class OpeningPlanController < ApplicationController
 
   private
 
-  def build_opening_plan_from_inventory(current_plan)
-    current_inventory.datasets.each do |element|
-      next if current_plan.map(&:name).include?(element.dataset_title)
-      build_opening_plans(element, current_plan) unless element.private?
-    end
-  end
-
-  def build_opening_plans(element, current_plan)
-    @organization.opening_plans.build do |plan|
-      plan.name = element.dataset_title
-      plan.publish_date = element.publish_date
-    end
-  end
-
-  def create_opening_plan_officials
-    @organization.opening_plans.each do |opening_plan|
-      administrator = create_opening_plan_administrator(opening_plan)
-      liaison = create_opening_plan_liaison(opening_plan)
-    end
-  end
-
-  def create_opening_plan_administrator(opening_plan)
-    user = @organization.administrator.try(:user)
-    return unless user
-    opening_plan.officials.create(email: user.email, name: user.name, kind: 'admin')
-  end
-
-  def create_opening_plan_liaison(opening_plan)
-    user = @organization.liaison.try(:user)
-    return unless user
-    opening_plan.officials.create(email: user.email, name: user.name, kind: 'liaison')
-  end
-
   def generate_opening_plan_dataset
     OpeningPlanDatasetGenerator.new(@organization.catalog).generate
-  end
-
-  def organization_params
-    params.require(:organization).permit(
-      opening_plans_attributes: [
-        :name,
-        :description,
-        :accrual_periodicity,
-        :publish_date,
-        :_destroy
-      ]
-    )
   end
 end

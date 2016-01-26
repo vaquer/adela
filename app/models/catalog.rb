c08111f91cddbfd11d67c0492b8aeda73d56eb59
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-26 12:16:39 -0600

    Removes the csv_file column from catalogs

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,11 +1,10 @@
 class Catalog < ActiveRecord::Base
   belongs_to :organization
+
   has_many :datasets, dependent: :destroy
   has_many :distributions, through: :datasets
 
   validates_presence_of :organization_id
 
-  # TODO: remove uploader after migrating data from csv to database
-  mount_uploader :csv_file, FileUploader
   accepts_nested_attributes_for :datasets
 end

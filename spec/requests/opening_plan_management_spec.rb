7c0594e950da0c68db26958147ef7d4eaf27dc38
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-27 04:35:54 -0600

    Skips the opening plan api test
    The opening plan implementation will change.

--- spec/requests/opening_plan_management_spec.rb
+++ spec/requests/opening_plan_management_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 
-feature "opening plan management" do
-  let(:opening_plan) { create(:opening_plan_with_officials) }
-
-  it "returns the organization opening plan" do
+feature 'opening plan management' do
+  it 'returns the organization opening plan', skip: true do
     get "/#{opening_plan.organization.slug}/plan.json"
-    expect(response).to match_response_schema("opening_plan")
+    expect(response).to match_response_schema('opening_plan')
   end
 end

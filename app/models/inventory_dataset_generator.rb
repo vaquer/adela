b4429f1563135427fbf3eaeda8e74a44aa7d93be
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-01-27 04:26:25 -0600

    Fixes user_sees_inventory_elements spec

--- app/models/inventory_dataset_generator.rb
+++ app/models/inventory_dataset_generator.rb
@@ -71,6 +71,7 @@ class InventoryDatasetGenerator
       dataset.accrual_periodicity = 'irregular'
       dataset.publish_date = DateTime.new(2015, 8, 28)
       dataset.editable = false
+      dataset.published = true
     end
   end
 

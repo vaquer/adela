992c1871717eea0d3b05af5b80a020a6a98809fb
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-02-23 12:22:06 -0600

    Relplaces the ActivityLog#done_at with #created_at

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,7 +2,7 @@ class ActivityLog < ActiveRecord::Base
 
   belongs_to :organization
 
-  scope :date_sorted, -> { order("activity_logs.done_at DESC") }
+  scope :date_sorted, -> { order("created_at DESC") }
   scope :first_block, -> { date_sorted.limit(5) }
   scope :last_block, -> { date_sorted.limit(10).offset(5) }
 end

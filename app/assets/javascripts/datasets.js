db2d353e1edd156a4d1255d6d72303c59b78cbce
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-03-13 13:03:27 -0600

    Adds the media type select form for distributions

--- app/assets/javascripts/datasets.js
+++ app/assets/javascripts/datasets.js
@@ -24,3 +24,21 @@ $(function () {
     setRequiredFields();
     $('#dataset_public_access').change(setRequiredFields);
 });
+
+$(document).on('nested:fieldAdded', function(event){
+    var updateMediaType;
+    (updateMediaType = function() {
+      var mediaType = event.field.find('.form-group .media_type_select');
+      var inputText = event.field.find('.form-group input.media_type');
+
+      inputText.val(mediaType.val());
+      if (mediaType.val() == 'otro') {
+        inputText.val('');
+        inputText.show();
+      } else {
+        inputText.hide();
+      }
+    })();
+
+    $('.media_type_select').on('change', updateMediaType);
+})

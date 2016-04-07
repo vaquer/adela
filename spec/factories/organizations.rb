ff4232040e2209e4b800b3921156db67109d9763
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-04-07 10:28:04 -0500

    Fixes the organization factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     landing_page { Faker::Internet.url }
 
     after(:build) do |organization|
-      build(:inventory, organization: organization)
+      build(:inventory, organization: organization, authorization_file: nil, designation_file: nil)
     end
 
     trait :federal do

0bfb3a18ea9eea264a6d69ad36b69a10c712715b
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-04-21 14:06:11 -0500

    Adds the catalog_with_documented_datasets factory

--- spec/factories/catalogs.rb
+++ spec/factories/catalogs.rb
@@ -19,5 +19,15 @@ FactoryGirl.define do
         create_list(:catalog_dataset, evaluator.datasets_count, catalog: catalog)
       end
     end
+
+    factory :catalog_with_documented_datasets do
+      transient do
+        datasets_count { Faker::Number.between(1, 8) }
+      end
+
+      after(:create) do |catalog, evaluator|
+        create_list(:dataset, evaluator.datasets_count, catalog: catalog)
+      end
+    end
   end
 end

7a3abfc294eaad82871e89f0e5e96978b950920e
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-04-22 12:02:03 -0500

    Adds the dct:issued to the dataset model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421230940) do
+ActiveRecord::Schema.define(version: 20160422164828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20160421230940) do
     t.string   "contact_position"
     t.boolean  "public_access",       default: true
     t.boolean  "editable",            default: true
+    t.datetime "issued"
   end
 
   add_index "datasets", ["catalog_id"], name: "index_datasets_on_catalog_id", using: :btree

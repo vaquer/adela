57491cc4121a062ea31b7e0f513668870a229771
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-05-08 18:40:56 -0500

    Autofills the distribution publish date with the dataset publish date

--- app/assets/javascripts/datasets.js
+++ app/assets/javascripts/datasets.js
@@ -26,21 +26,27 @@ $(function () {
 });
 
 $(document).on('nested:fieldAdded', function(event){
-    var updateMediaType;
-    (updateMediaType = function () {
-      var mediaType = event.field.find('.form-group .media_type_select option:selected').val();
-      var format = event.field.find('.form-group .media_type_select option:selected').text();
+    var updateMediaType, datasetPublishDate;
 
-      if (format === 'otro') {
+    (updateMediaType = function() {
+      var mediaType = event.field.find('.form-group .media_type_select');
+      var inputText = event.field.find('.form-group input.media_type');
+      var format    = event.field.find('.form-group .media_type_select option:selected').text();
+
+      if (format == 'otro') {
         event.field.find('.form-group input.media_type').val('');
         event.field.find('.form-group input.format').val('');
         event.field.find('.form-group input.format').parent().show();
       } else {
-        event.field.find('.form-group input.media_type').val(mediaType);
+        event.field.find('.form-group input.media_type').val(mediaType.val());
         event.field.find('.form-group input.format').val(format);
         event.field.find('.form-group input.format').parent().hide();
       }
     })();
+
+    // sets dataset publish-date into new distribution
+    datasetPublishDate = $('#dataset_publish_date').val();
+    event.field.find('.publish-date').val(datasetPublishDate);
 
     $('.media_type_select').on('change', updateMediaType);
     $('.datepicker').datepicker();

ed158f6bbbd0a29fe03235ce845badf0f912fb2f
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-05-10 10:46:51 -0500

    Adds the Inventory Serializer

---
+++ app/serializers/inventories/distributions_serializer.rb
@@ -0,0 +1,20 @@
+class Inventories::DistributionsSerializer < ActiveModel::Serializer
+  attributes :title, :description, :downloadURL, :mediaType, :byteSize, :temporal, :spatial,
+             :license, :issued
+
+  def license
+    'http://datos.gob.mx/libreusomx/'
+  end
+
+  def downloadURL
+    object.download_url
+  end
+
+  def mediaType
+    object.media_type
+  end
+
+  def byteSize
+    object.byte_size
+  end
+end

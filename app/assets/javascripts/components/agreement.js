7c50a56dd81806c173f7ebb13a557abf8e531de8
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2016-06-02 23:33:29 -0500

    Fixes the disabled buttons

--- app/assets/javascripts/components/agreement.js
+++ app/assets/javascripts/components/agreement.js
@@ -1,7 +1,9 @@
 var termsConfirmed = function() {
   if( $('#agreement-publish').is(':checked') && $('#agreement-validated').is(':checked') ){
     $('#publish').removeClass('disabled');
+    $('#publish').prop('disabled', false);
   } else {
     $('#publish').addClass('disabled');
+    $('#publish').prop('disabled', true);
   }
 };

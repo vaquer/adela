34887176b30a1240db63f0cae6e08d3f5ddcbeb8
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-06-16 17:18:23 -0500

    Adds the imageUrl field into the OrganizationSerializer

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,3 +1,8 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :title, :slug, :description, :gov_type, :created_at, :updated_at
+  attributes :title, :slug, :description, :gov_type, :created_at, :updated_at,
+             :imageUrl
+
+  def imageUrl
+    "https://raw.githubusercontent.com/mxabierto/assets/master/img/logos/#{object.slug}.png"
+  end
 end

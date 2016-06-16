5f0a5c38f5895f983b76bd8738510a27e61f4eb6
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-06-16 12:15:14 -0500

    Updates the style of the OrganizationSerializer

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,6 +1,10 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :title, :slug, :description, :gov_type, :created_at, :updated_at,
+  attributes :title, :slug, :description, :govType, :created_at, :updated_at,
              :imageUrl
+
+  def govType
+    object.gov_type
+  end
 
   def imageUrl
     "https://raw.githubusercontent.com/mxabierto/assets/master/img/logos/#{object.slug}.png"

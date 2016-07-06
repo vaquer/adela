cb7352a334ae4ad249d44c89ab814244db8aa411
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-07-05 22:12:08 -0500

    Adds the carrierwave-ftp configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,5 +3,13 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :ftp
+    config.ftp_host = ENV['FTP_HOST']
+    config.ftp_port = ENV['FTP_PORT']
+    config.ftp_user = ENV['FTP_USER']
+    config.ftp_passwd = ENV['FTP_PASSWD']
+    config.ftp_folder = ENV['FTP_FOLDER']
+    config.ftp_url = ENV['FTP_URL']
   end
 end

56003fcfc9576302b4f1e8c3f2c19230e8ec30c8
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-08-02 12:28:34 -0500

    Moves some helpers to Concerns::PublishableHelper

--- app/helpers/distributions_helper.rb
+++ app/helpers/distributions_helper.rb
@@ -2,15 +2,4 @@ module DistributionsHelper
   def options_for_media_type
     options_for_select(I18n.t('media_type'))
   end
-
-  def state_description(distribution)
-    case distribution.state
-    when 'documented', 'refined'
-      'Listo para publicar'
-    when 'published'
-      'Publicado'
-    else
-      'Falta información'
-    end
-  end
 end

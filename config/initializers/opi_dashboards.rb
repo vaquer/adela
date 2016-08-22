5de398a8abcc6270665f94c6e2ac52b4f9721c35
Francisco Vaquero C <FranciscoVaquero@iMac-de-Francisco.local>
2016-08-22 17:30:30 -0500

    Agregando initializer con URLS para los dashboards de las dependencias e intgreacion en vista dashboards

---
+++ config/initializers/opi_dashboards.rb
@@ -0,0 +1,3 @@
+OPI_DASHBOARD_URL_API = ENV['DATASET_OPI'] || 'http://10.15.9.13:8001/tablacomparativa/apicomparativa/'
+OPI_BUDA_URL_API = ENV['DATASET_BUDA'] || 'http://api.datos.gob.mx/v1/data-fusion?adela.inventory.slug='
+OPI_LOGOS_URL = ENV['LOGOS_URL'] || 'https://raw.githubusercontent.com/mxabierto/assets/master/img/logos/'

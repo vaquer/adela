07638d0eff3d4d6a5042526f006f97de624f9a98
esaugtz <dgesau@gmail.com>
2016-08-26 16:02:31 -0500

    Se eliminan comentarios y cambio de nombre de variable por convención

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -44,9 +44,7 @@ module Admin
 
     def update_multiple
       authorize! :update, Organization
-      @funciono = Organization.update(params[:organizations].keys, params[:organizations].values)
-      #render :nothing
-      #redirect_to edit_multiple_admin_organizations_path, notice: 'Cambios guardados'
+      @success = Organization.update(params[:organizations].keys, params[:organizations].values)
     end
 
     private

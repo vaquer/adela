d9f87b442197895b4951ff63fe3f2deaad902d37
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-09-02 10:58:36 -0500

    Toggles hidden class on the warning message

--- app/assets/javascripts/components/agreement.js
+++ app/assets/javascripts/components/agreement.js
@@ -1,7 +1,9 @@
 var termsConfirmed = function() {
   if( $('#agreement-publish').is(':checked') && $('#agreement-validated').is(':checked') ){
     $('#publish').removeClass('disabled');
+    $('#adela-message').toggleClass('hidden');
   } else {
     $('#publish').addClass('disabled');
+    $('#adela-message').addClass('hidden');
   }
 };

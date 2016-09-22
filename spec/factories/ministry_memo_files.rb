0e228c77cc2b15a6cd14c28024c3102b0e8d8a90
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-09-22 13:16:04 -0500

    Adds the Ministry Memo Files into the Organization Files

--- spec/factories/ministry_memo_files.rb
+++ spec/factories/ministry_memo_files.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :ministry_memo_file do
-    file { File.new("#{Rails.root}/spec/fixtures/files/memo_file.pdf") }
+    file { File.new("#{Rails.root}/spec/fixtures/files/ministry_memo_file.pdf") }
     organization
   end
 end

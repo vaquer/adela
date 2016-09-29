454d57e8c8f872ae5b803408bf58c1f44fbec55c
KielRodriguez <kr.rdz.20@gmail.com>
2016-09-29 05:40:00 -0500

    Se agrego validacion para evitar fecha de inicio de periodo sea mayor que la del final

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -7,6 +7,7 @@ class Distribution < ActiveRecord::Base
 
   validates_uniqueness_of :title
   validates_uniqueness_of :download_url, allow_nil: true
+  validate :validate_temporal
 
   has_one :catalog, through: :dataset
   has_one :organization, through: :dataset
@@ -40,5 +41,15 @@ class Distribution < ActiveRecord::Base
     def update_dataset_metadata
       return unless dataset
       dataset.update_attribute(:modified, modified) if dataset.modified < modified
+    end
+
+    #Método valida existencia del valor asi como el periodo
+    #inico menor a  periodo final
+    def validate_temporal
+        tmps = self.temporal.split('/')
+        inicio = Date.strptime(tmps[0], '%Y-%m-%d')
+        final = Date.strptime(tmps[1], '%Y-%m-%d')
+
+        errors.add(:temporal, "El valor del campo \"Inicio del período de tiempo cubierto\" debe ser menor al de \"Fin del período de tiempo cubierto\" ") if inicio > final
     end
 end

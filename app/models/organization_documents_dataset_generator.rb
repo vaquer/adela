a5e0e6c9ef33e627137a3fad156ee53ba413ee69
Luis Alfredo Lorenzo <babasbot@gmail.com>
2016-10-13 11:24:27 -0500

    Creates the OrganizationDocumentsDatasetGenerator class

---
+++ app/models/organization_documents_dataset_generator.rb
@@ -0,0 +1,46 @@
+class OrganizationDocumentsDatasetGenerator
+  include Rails.application.routes.url_helpers
+
+  def initialize(organization)
+    @organization = organization
+  end
+
+  def generate!
+    dataset = build_dataset
+    dataset.sector = Sector.friendly.find('otros')
+    build_distribution(dataset)
+    dataset.save
+  end
+
+  private
+
+    def build_dataset
+      @organization.catalog.datasets.build do |dataset|
+        dataset.title = "Oficios y Documentos Institucionales de #{@organization.title}"
+        dataset.description = "Contiene el Oficio de designación del Enlace y Administrador de Datos vigente, "\
+        "así como las minutas celebradas por el Consejo Institucional de Datos Abierto de #{@organization.title}."
+        dataset.accrual_periodicity = 'irregular'
+        dataset.publish_date = Time.current + 1.week
+        dataset.contact_position = ENV_CONTACT_POSITION_NAME
+        dataset.mbox = @organization.administrator&.user&.email
+        dataset.temporal = "#{Date.current.iso8601}/#{Date.new(2018, 11, 30).iso8601}"
+        dataset.keyword = 'oficio,minutas,documentos'
+        dataset.landing_page = 'http://datos.gob.mx/guia/docs/oficio_designacion_enlace_administrador.docx'
+        dataset.editable = false
+      end
+    end
+
+    def build_distribution(dataset)
+      dataset.distributions.build do |distribution|
+        distribution.title = "Oficios y Documentos Institucionales de #{@organization.title}"
+        distribution.description = "Contiene el Oficio de designación del Enlace y Administrador de Datos vigente, "\
+        "así como las minutas celebradas por el Consejo Institucional de Datos Abierto de #{@organization.title}."
+        distribution.download_url = "http://datos.gob.mx#{documents_api_v1_organization_path(@organization)}"
+        distribution.media_type = 'application/json'
+        distribution.format = 'json'
+        distribution.modified = Time.current
+        distribution.temporal = "#{Date.current.iso8601}/#{Date.new(2018, 11, 30).iso8601}"
+        distribution.publish_date = Time.current + 1.week
+      end
+    end
+end

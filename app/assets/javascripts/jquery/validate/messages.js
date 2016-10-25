6a3054525223ac97bbedc37279890a1bef73a69a
KielRodriguez <kr.rdz.20@gmail.com>
2016-10-25 11:16:34 -0500

    Mensaje de error para correo invalido fue modificado para mostrar en español

--- app/assets/javascripts/jquery/validate/messages.js
+++ app/assets/javascripts/jquery/validate/messages.js
@@ -1,5 +1,6 @@
 jQuery.extend(jQuery.validator.messages, {
     required: "Completa este campo",
     url: "Ingresa una URL valida",
-    dateISO: "Ingresa una fecha valida en formato ISO-8601"
+    dateISO: "Ingresa una fecha valida en formato ISO-8601",
+    email: "Ingresa un correo electrónico valido"
 });

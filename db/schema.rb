4a35367431874ffefe4f6d46e9adb012dbbc4b86
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-05-16 16:59:12 -0500

    Fixes the invalid titles on datasets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516173615) do
+ActiveRecord::Schema.define(version: 20170516174341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

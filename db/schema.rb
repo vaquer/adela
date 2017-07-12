a4c6b00d0c254e4086bb54ed7a51c7b0203e5a3c
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-07-12 01:06:54 -0500

    Splits the temporal data into the init_date & term_date column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712053003) do
+ActiveRecord::Schema.define(version: 20170712054823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

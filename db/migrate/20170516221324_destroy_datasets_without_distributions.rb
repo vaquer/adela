710085b7a8ec3eab06775d692445dcc8f00196d4
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-07-18 19:36:10 -0500

    Fixes a broken migration

--- db/migrate/20170516221324_destroy_datasets_without_distributions.rb
+++ db/migrate/20170516221324_destroy_datasets_without_distributions.rb
@@ -1,4 +1,7 @@
 class DestroyDatasetsWithoutDistributions < ActiveRecord::Migration
+  class Dataset < ActiveRecord::Base
+  end
+
   def change
     Dataset.includes(:distributions).where(distributions: { id: nil }).destroy_all
   end
